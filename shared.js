b611c48ea1e29af1d00a0e404285fc5bb34382cb
jogonpav <josepabonvera@gmail.com>
2022-10-02 13:10:23 -0500

    Added hamburguer menu

--- shared.js
+++ shared.js
@@ -2,6 +2,8 @@ let backdrop = document.querySelector('.backdrop');
 let modal = document.querySelector('.modal')
 let modalNoButton = document.querySelector('.modal__action--negative');
 let selectPlanButton = document.querySelectorAll('.plan button');
+let toggleButtom = document.querySelector('.toggle-button');
+let mobileNav = document.querySelector('.mobile-nav')
 
 
 for (let i = 0; i < selectPlanButton.length; i++) {
@@ -11,14 +13,31 @@ for (let i = 0; i < selectPlanButton.length; i++) {
 
 modalNoButton.addEventListener('click', closeModal);
 
-backdrop.addEventListener('click', closeModal);
+backdrop.addEventListener('click', function () {
+    //mobileNav.style.display = 'none';
+    mobileNav.classList.remove('open')
+
+    closeModal();
+});
 
 function openModal() {
-    modal.style.display = 'block';
-    backdrop.style.display = 'block';
+    //modal.style.display = 'block';
+    //backdrop.style.display = 'block';
+    //modal.className = 'open'; //this will actually overwrite the complete class list
+    modal.classList.add('open');
+    backdrop.classList.add('open');
 }
 
 function closeModal() {
-    modal.style.display = 'none';
-    backdrop.style.display = 'none';
+    //modal.style.display = 'none';
+    //backdrop.style.display = 'none';
+    modal.classList.remove('open');
+    backdrop.classList.remove('open');
 }
+
+toggleButtom.addEventListener('click', function () {
+    //mobileNav.style.display = 'block';
+    //backdrop.style.display = 'block';
+    mobileNav.classList.add('open');
+    backdrop.classList.add('open');
+});
